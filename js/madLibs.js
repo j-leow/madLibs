a19b4fa077408be8fb6d9de24ac4699e2be3df7b
j-leow <jleow@iu.edu>
2018-04-07 23:43:56 -0700

    Initial upload

---
+++ js/madLibs.js
@@ -0,0 +1,10 @@
+/**************************************
+ TITLE: madLibs.js
+ AUTHOR: Jules Leow
+ CLASS: NEWM-N220
+ CREATE DATE: 03/07/2018
+ Purpose: Create a page where users can input a noun, a verb, and a place and the story generator takes those inputs and sets them in the story. Use local storage to persist data. Use string replace.
+ Date: 04/07/2018
+ Modification History:
+ 04/07/2018: Original Build
+***************************************/
